0a1219e82058f7ec99d2be31db64a1ab2ec62e11
Peter Young <young40@gmail.com>
2020-08-11 23:57:47 +0800

    add move object

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MoveObject : MonoBehaviour
+{
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidbody2d;
+    private float inverseMoveTime;
+    private bool isMoving;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        inverseMoveTime = 1f / moveTime;
+    }
+
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+
+        if (hit.transform == null && !isMoving)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+
+        return false;
+    }
+
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        isMoving = true;
+
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while (sqrRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rigidbody2d.position, end, inverseMoveTime * Time.deltaTime);
+            rigidbody2d.MovePosition(newPosition);
+
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+
+    protected abstract void OnCantMove<T>(T component) where T : Component;
+}
